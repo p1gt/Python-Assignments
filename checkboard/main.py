5b9c38d91b8324120f05900d9c9bdabeb25f4f1e
p1gt <ranaranap59@gmail.com>
2023-05-28 19:40:45 +0300

    updated my code

--- checkboard/main.py
+++ checkboard/main.py
@@ -2,16 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return render_template('main.html')
-
-@app.route('/4')
-def halfCheckboard():
-    return render_template('four.html')
-
 @app.route('/<int:x>')
-def rows(x):
-    return render_template('row.html', x=x)
+@app.route('/<int:x>/<int:y>')
+def checkerboard(x=8, y=8):
+    return render_template('index.html', rows=x, cols=y)
 
-if __name__==('__main__'):
+if __name__=="__main__":  
     app.run(debug=True)
