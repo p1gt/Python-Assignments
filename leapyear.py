e2124c8b9d6ee84e17513318c4ebdd445405d4bd
p1gt <ranaranap59@gmail.com>
2023-05-28 22:22:38 +0300

    factoring code

--- leapyear.py
+++ leapyear.py
@@ -1,17 +1,7 @@
-def isLeapYear(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return 1
-            else:
-                return 0
-        else:
-            return 1
-    else:
+def isLeap(year):
+    if year % 4 != 0:
         return 0
-print(isLeapYear(2024))
-print(isLeapYear(1992))
-print(isLeapYear(1700))
-print(isLeapYear(1600))
-print(isLeapYear(2002))
-print(isLeapYear(-5))
+    elif year % 100 == 0 and year % 400 != 0:
+        return 0
+    else:
+        return 1
