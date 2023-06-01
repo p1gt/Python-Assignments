d9bb86b9d9e340be497754012ca9cbadde14a75f
p1gt <ranaranap59@gmail.com>
2023-06-01 15:09:52 +0300

    usertemplates

--- django/single_model_orm/users_app/models.py
+++ django/single_model_orm/users_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+
 class Users(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email_address = models.EmailField(max_length=245)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
